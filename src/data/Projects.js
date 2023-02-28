bf2eb472ddbeff7fc3083f817b6a20ce49fc564d
Matthew Gilligan <mcgilligan97@gmail.com>
2023-02-28 09:40:59 +0900

    Add more assets

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -5,17 +5,18 @@ export const Projects = [
     url: 'https://newedgestud.io',
     homeImage: '/projects/new-edge-home.webp',
     desktopImages: [
-      '/projects/the-glow-desktop-1.webp',
-      '/projects/the-glow-desktop-2.webp',
+      '/projects/new-edge-desktop-1.webp',
+      '/projects/new-edge-desktop-2.webp',
     ],
     mobileImages: [
       '/projects/the-glow-mob-1.webp',
       '/projects/the-glow-mob-2.webp',
+      '/projects/the-glow-mob-3.webp',
     ],
     year: '2023',
     team: ['Evan Oliver', 'Nic Reeves'],
     category: ['UX/UI Design', 'Web Development'],
-    desc: 'ACT Responsable is a french association created to identify and collect the best communications campaigns around the world in order to raise awareness on social and environmental issues (solidarity, environment, human rights and education). The aim was to redesign the french website by focusing on their actions which are exhibitions, schools interventions or even the NG’Ad club.',
+    desc: 'NewEdge Studio is a marketing agency based in London focused mainly on branding and web. The brief was to create a clean, yet memorable website which would showcase their previous work and attract new clients.',
   },
 
   {
@@ -30,10 +31,11 @@ export const Projects = [
     mobileImages: [
       '/projects/the-glow-mob-1.webp',
       '/projects/the-glow-mob-2.webp',
+      '/projects/the-glow-mob-3.webp',
     ],
     year: '2022',
     category: ['Branding', 'UX/UI Design', 'Web Development'],
-    desc: 'ACT Responsable is a french association created to identify and collect the best communications campaigns around the world in order to raise awareness on social and environmental issues (solidarity, environment, human rights and education). The aim was to redesign the french website by focusing on their actions which are exhibitions, schools interventions or even the NG’Ad club.',
+    desc: 'The Glow is an English language Japanese music publication, aimed at introudcing Japanese music to an English speaking audience. The original code base was quite messy, and quite difficult to maintain, so the aim was to refactor the code into maintainable modern components and update the design to reflect the change in content direction.',
   },
   {
     clientName: 'SciLeads',
@@ -41,17 +43,18 @@ export const Projects = [
     url: 'https://scileads.com',
     homeImage: '/projects/scileads-home.webp',
     desktopImages: [
-      '/projects/the-glow-desktop-1.webp',
-      '/projects/the-glow-desktop-2.webp',
+      '/projects/scileads-desktop-1.webp',
+      '/projects/scileads-desktop-2.webp',
     ],
     mobileImages: [
       '/projects/scileads-mob-1.webp',
       '/projects/scileads-mob-2.webp',
+      '/projects/scileads-mob-3.webp',
     ],
     year: '2021',
     team: ['Cait Skinner'],
     category: ['Web Development'],
-    desc: 'SciLeads is a lead generation service in the life sciences industry.',
+    desc: 'SciLeads is a lead generation service in the life sciences industry. Data protection is of the utmost importance to SciLeads, so the challenge was to create a self-hosted CSM which was only accessible behind the companys VPN, but was also accessible to deployment pipelines. Contact forms and third-party APIs were all routed through the company .NET API which I maintained.',
   },
   {
     clientName: 'Second Life',
@@ -59,15 +62,16 @@ export const Projects = [
     url: 'https://secondlifefp.co.uk',
     homeImage: '/projects/second-life-home.webp',
     desktopImages: [
-      '/projects/the-glow-desktop-1.webp',
-      '/projects/the-glow-desktop-2.webp',
+      '/projects/second-life-desktop-1.webp',
+      '/projects/second-life-desktop-2.webp',
     ],
     mobileImages: [
-      '/projects/the-glow-mob-1.webp',
-      '/projects/the-glow-mob-2.webp',
+      '/projects/second-life-mob-1.webp',
+      '/projects/second-life-mob-2.webp',
+      '/projects/second-life-mob-3.webp',
     ],
     year: '2022',
     category: ['UX/UI Design', 'Web Development'],
-    desc: 'ACT Responsable is a french association created to identify and collect the best communications campaigns around the world in order to raise awareness on social and environmental issues (solidarity, environment, human rights and education). The aim was to redesign the french website by focusing on their actions which are exhibitions, schools interventions or even the NG’Ad club.',
+    desc: "Second Life is a British financial planning service. The goal was to create a website that would reflect the company's commitment to transparency and trust. Due to the older clientelle of Second Life, an emphasis was put on simple and accessible design.",
   },
 ];
